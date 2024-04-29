f0a7fdb40c7718b597e79ac6fb8189aeb38dcc58
Grégoire Denay <gregoire.denay@cvua-rrw.de>
2024-04-29 15:33:37 +0200

    Format and clean-up filter_blast.py

--- bin/filter_blast.py
+++ bin/filter_blast.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
 import sys
 import argparse
 from os import stat
 import pandas as pd
 
+
 parser=argparse.ArgumentParser(description="Script options")
 parser.add_argument("--report")
 parser.add_argument("--output")
-parser.add_argument("--bit_diff", type=int)
-args=parser.parse_args()
+parser.add_argument("--bit_diff", type=int, default=4)
+args = parser.parse_args()
 
-bit_diff = args.bit_diff if args.bit_diff else 4
 
-str = "\t"
-header = [ "query","subject","evalue","identity","bitscore","subject_acc","subject_taxid","alignment_length","mismatch","gaps","subject_name" ]
+def main(report, output, bit_diff):
+    str = "\t"
+    header = [
+        "query",
+        "subject",
+        "evalue",
+        "identity",
+        "bitscore",
+        "subject_acc",
+        "subject_taxid",
+        "alignment_length",
+        "mismatch",
+        "gaps",
+        "subject_name"
+    ]
 
-if stat(args.report).st_size == 0:
-    with open(args.output,"w") as fout:
-        fout.write(
-            str.join(header)
-        )
-else:
-    df = pd.read_csv(args.report, sep="\t", names = header )
-    if df.empty:
-        df.to_csv(args.output, sep="\t", header=True, index=False)
-    else:
-        sd = dict(tuple(df.groupby("query")))
-        dfout = pd.DataFrame()
-        for key, val in sd.items():
-            dfout = pd.concat(
-                [dfout, val[val["bitscore"] >= max(val["bitscore"]) - bit_diff]]
+    if stat(report).st_size == 0:
+        with open(output,"w") as fout:
+            fout.write(
+                str.join(header)
             )
-        dfout["query"] = dfout["query"].str.split(";").str[0]
-        dfout.to_csv(args.output, sep="\t", header=True, index=False) 
+    else:
+        df = pd.read_csv(report, sep="\t", names = header )
+        if df.empty:
+            df.to_csv(output, sep="\t", header=True, index=False)
+        else:
+            sd = dict(tuple(df.groupby("query")))
+            dfout = pd.DataFrame()
+            for key, val in sd.items():
+                dfout = pd.concat(
+                    [dfout, val[val["bitscore"] >= max(val["bitscore"]) - bit_diff]]
+                )
+            dfout["query"] = dfout["query"].str.split(";").str[0]
+            dfout.to_csv(output, sep="\t", header=True, index=False)
+
+
+if __name__ == '__main__':
+    main(args.report, args.output, args.bit_diff)
